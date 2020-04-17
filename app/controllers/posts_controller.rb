02aa35a49cdef624e239bea5736a92a19786c0e1
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-17 21:23:42 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ def post_params(*args)
   params.require(:post).permit(*args)
 end
 
-	def edit
-	  @post = Post.find(params[:id])
-	end
+# 	def edit
+# 	  @post = Post.find(params[:id])
+# 	end
 end
